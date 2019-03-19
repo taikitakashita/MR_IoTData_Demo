3ef60ca594039b68d9de92d3df80399ddf3d209f
taiki.takashita <taiki.takashita@qdenbs.com>
2019-03-19 11:21:09 +0900

    ・初めてのコミット

---
+++ Assets/Scripts/AzureServices.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using System.IO;
+using System.Net;
+using System.Collections;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+public class AzureServices : MonoBehaviour
+{
+    /// <summary>
+    /// このクラスをシングルトンのようにふるまわせるるためのインスタンス
+    /// </summary>
+    public static AzureServices Instance;
+
+    /// <summary>
+    /// AzureFunctionのエンドポイントのURL
+    /// </summary>
+    private readonly string azureFunctionEndpoint = "https://mriottestfunctions.azurewebsites.net/api/HttpTrigger1";
+
+    private void Awake()
+    {
+        // このクラスをシングルトンとしてふるまわせる
+        Instance = this;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        // （UnityがTLSのサポートを追加するまで）UnityエディタでのみTLS証明書チェックを無効にする
+    #if UNITY_EDITOR
+        ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+    #endif
+    }
+
+    /// <summary>
+    /// AzureFunctionの処理を呼び出す
+    /// </summary>
+    public IEnumerator CallAzureFunction()
+    {
+        Debug.Log("センサー情報取得処理開始");
+        SensorInformationEntity sensorinformationentity = null;
+
+        using (UnityWebRequest www = UnityWebRequest.Get(azureFunctionEndpoint))
+        {
+            // 受信したデータを格納するハンドラの初期化
+            www.downloadHandler = new DownloadHandlerBuffer();
+            //Debug.Log("++++++++++++++++++++1");
+
+            // 送受信を開始し、完了するまで待つ
+            yield return www.SendWebRequest();
+            //Debug.Log("++++++++++++++++++++2");
+
+            // ダウンロードした内容を文字列として取得
+            string jsonResponse = www.downloadHandler.text;
+            //Debug.Log("++++++++++++++++++++3");
+
+            // 取得したJSON文字列を逆シリアライズ化
+            sensorinformationentity = JsonConvert.DeserializeObject<SensorInformationEntity>(jsonResponse);
+            //Debug.Log("++++++++++++++++++++4");
+        }
+        SensorInformationManager.Instance.SetSensorInformation(sensorinformationentity);
+        //Debug.Log("++++++++++++++++++++5");
+        Debug.Log("センサー情報取得及び分析処理完了");
+    }
+}
---
+++ Assets/Scripts/SensorInformationManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorInformationManager : MonoBehaviour {
+
+    /// <summary>
+    /// このクラスをシングルトンとしてふるまわせるためのインスタンス
+    /// </summary>
+    public static SensorInformationManager Instance;
+
+    /// <summary>
+    /// センサーの値を表示するTemperatureScoreとHumidityScoreのLabelをインスペクターから設定
+    /// </summary>
+    public TextMesh TemperatureScoreLabel;
+    public TextMesh HumidityScoreLabel;
+
+    /// <summary>
+    /// センサー値の閾値超過を表示するArarmMesaageのLabelをインスペクターから設定
+    /// </summary>
+    public TextMesh TemperatureArarmLabel;
+    public TextMesh HumidityArarmLabel;
+
+    // センサー値の閾値超過を判定する基準
+    private int TemperatureThreshold = 30;
+    private int HumidityThreshold = 70;
+
+    //センサー情報取得処理を行う間隔
+    private float IntervalTime = 5.0f;
+
+    /// <summary>
+    /// Initialises this class
+    /// </summary>
+    private void Awake()
+    {
+        // このクラスのインスタンスをシングルトンとして利用する
+        Instance = this;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        // センサーの値を表示するTemperatureScoreとHumidityScoreのLabelの初期値を表示
+        TemperatureScoreLabel.text = "取得中...";
+        HumidityScoreLabel.text = "取得中...";
+
+        // センサー値の閾値超過を表示するArarmMesaageのLabelの初期値を表示
+        TemperatureArarmLabel.text = "";
+        HumidityArarmLabel.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IntervalTime = IntervalTime + Time.deltaTime;
+        //Debug.Log(IntervalTime);
+        if (IntervalTime >= 5.0f)
+        {
+            StartCoroutine(AzureServices.Instance.CallAzureFunction());
+            IntervalTime = 0;
+        }
+    }
+
+    public void SetSensorInformation(SensorInformationEntity entity)
+    {
+        double ShowTemperatureScore = (double)Math.Round(entity.temperature, 2, MidpointRounding.AwayFromZero);
+        double ShowHumidityScore = (double)Math.Round(entity.humidity, 2, MidpointRounding.AwayFromZero);
+
+        TemperatureScoreLabel.text = ShowTemperatureScore.ToString();
+        HumidityScoreLabel.text = ShowHumidityScore.ToString();
+
+        if (ShowTemperatureScore > TemperatureThreshold)
+        {
+            TemperatureArarmLabel.text = "温度は閾値を超過しました。";
+            TemperatureArarmLabel.color = Color.red;
+        }
+        else
+        {
+            TemperatureArarmLabel.text = "温度は正常です";
+            TemperatureArarmLabel.color = Color.green;
+        }
+
+        if (ShowHumidityScore > HumidityThreshold)
+        {
+            HumidityArarmLabel.text = "湿度は閾値を超過しました。";
+            HumidityArarmLabel.color = Color.red;
+        }
+        else
+        {
+            HumidityArarmLabel.text = "湿度は正常です";
+            HumidityArarmLabel.color = Color.green;
+        }
+    }
+}
+
+/// <summary>
+/// 取得したセンサ情報を格納するクラス
+/// </summary>
+public class SensorInformationEntity
+{
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public string deviceid { get; set; }
+    public DateTime eventenqueuedutctime { get; set; }
+    public double humidity { get; set; }
+    public double temperature { get; set; }
+}
